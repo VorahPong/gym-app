f3ecdc376e17f5b4dd850ba07dbcf0ef0e4f42a5
VorahPong <vorahpongmean@gmail.com>
2025-05-31 17:41:48 -0500

    Added cookie when login

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import bcrypt from 'bcryptjs';
+import { serialize } from 'cookie';
+import { NextResponse } from 'next/server';
 
 const prisma = new PrismaClient();
 
@@ -12,13 +14,26 @@ export async function POST(request: Request) {
     });
 
     if (!user) {
-        return Response.json({ message: 'Account does not exist or incorrect password'}, { status: 404});
+        return NextResponse.json({ message: 'Account does not exist or incorrect password'}, { status: 404});
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-        return Response.json({ message: 'Account does not exist or incorrect password'}, { status: 401});
+        return NextResponse.json({ message: 'Account does not exist or incorrect password'}, { status: 401});
     }
 
-    return Response.json({ message: 'Successfully login', user}, {status: 200 });
+    // set cookie
+    const cookie = serialize('userEmail', user.email, {
+        httpOnly: true,
+        path: '/',
+        maxAge: 60 * 60 * 24, // 1 day session
+    });
+
+    const response = NextResponse.json(
+        { message: 'Successfully login', user},
+        { status: 200 }
+    );
+    response.headers.set('Set-Cookie', cookie);
+
+    return response;
 }
