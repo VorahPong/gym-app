e36c7ea92d22b3ae4ee971b8d91920a1e5d1deef
VorahPong <vorahpongmean@gmail.com>
2025-05-31 17:42:09 -0500

    Redirect user to login page if not login

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,14 @@
-export default function DashboardPage() {
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function DashboardPage() {
+    const cookieStore = cookies();
+    const userEmail = (await cookieStore).get('userEmail');
+
+    if (!userEmail) {
+        redirect('/login');
+    }
+
     return (
         <div className="min-h-screen bg-white">
             hello
