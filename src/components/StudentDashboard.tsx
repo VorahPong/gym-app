614cd51564c44b3ba8d542821602ae8250a6cfe7
VorahPong <vorahpongmean@gmail.com>
2025-06-07 16:42:39 -0500

    Make StudentDashboard to be a client side

--- src/components/StudentDashboard.tsx
+++ src/components/StudentDashboard.tsx
@@ -1,7 +1,9 @@
+'use client'
+
 import ScanID from "@/components/ScanID";
 
 
-export default async function StudentDashboard({ studentId }: { studentId: string }) {
+export default function StudentDashboard({ studentId }: { studentId: string }) {
 
     return (
         <div className="min-h-screen bg-white text-black">
