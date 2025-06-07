e17b28f879cb657f5955e92c2692a09298fbb43a
VorahPong <vorahpongmean@gmail.com>
2025-06-07 16:04:27 -0500

    Bring the student features into its own dashboard

--- src/components/StudentDashboard.tsx
+++ src/components/StudentDashboard.tsx
@@ -1,3 +1,15 @@
-export default function StudentDashboard() {
-    
+import ScanID from "@/components/ScanID";
+
+
+export default async function StudentDashboard({ studentId }: { studentId: string }) {
+
+    return (
+        <div className="min-h-screen bg-white text-black">
+            <div className="flex">
+
+                {/* features */}
+                <ScanID studentId={studentId} />
+            </div>
+        </div>
+    );
 }
